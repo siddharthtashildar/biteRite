2330684e78f6cb47effb56954ee93b268867cc14
Sid <siddharthtashildar17@gmail.com>
2026-01-23 14:31:19 +0530

    Basic code structure

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,17 @@
+function RecipeCard({ recipe }) {
+  return (
+    <div className="bg-white rounded-2xl p-4 shadow-sm hover:shadow-md transition">
+      <div className="h-40  bg-gray-200 rounded-xl mb-4" />
+
+      <h3 className="font-semibold mb-1">{recipe.title}</h3>
+
+      <p className="text-sm text-gray-500 mb-3">{recipe.time}</p>
+
+      <button className="w-full py-2 text-sm bg-green-500 text-white rounded-lg">
+        View Recipe
+      </button>
+    </div>
+  );
+}
+
+export default RecipeCard;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,33 @@
+function Sidebar() {
+  return (
+    <div className="w-64 bg-white p-6 flex flex-col justify-between rounded-r-3xl">
+      <div>
+        <h2 className="text-2xl font-bold mb-8">BiteRite</h2>
+
+        <div className="flex items-center gap-3 mb-10">
+          <div className="w-12 h-12 rounded-full bg-gray-300" > 
+            <img src="src\assets\randomAhh\pfp.jpg" alt="pfp" className="w-full h-full rounded-full object-cover" />
+          </div>
+          <div>
+            <p className="font-medium">Jeffrey Epstien</p>
+            <p className="text-sm text-gray-500">  </p>
+          </div>
+        </div>
+
+        <nav className="space-y-4">
+          <button className="w-full text-left px-4 py-2 rounded-lg bg-yellow-400 font-medium">
+            Recipes
+          </button>
+          <button className="w-full text-left px-4 py-2 rounded-lg hover:bg-gray-100">
+            Favorites
+          </button>
+          <button className="w-full text-left px-4 py-2 rounded-lg hover:bg-gray-100">
+            Community
+          </button>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
